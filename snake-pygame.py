ff1a7321d9f15c03d34045e2c88cb6250c08d28a
Kenny Yip Coding <78777681+ImKennyYip@users.noreply.github.com>
2026-04-22 12:01:51 +0100

    Update snake-pygame.py

--- snake-pygame.py
+++ snake-pygame.py
@@ -5,27 +5,29 @@ import random
 ROWS = 25
 COLS = ROWS
 TILE_SIZE = 25
-WINDOW_WIDTH = TILE_SIZE * COLS #25*25 = 625
-WINDOW_HEIGHT = TILE_SIZE * ROWS #25*25 = 625
+GAME_WIDTH = TILE_SIZE * COLS #25*25 = 625
+GAME_HEIGHT = TILE_SIZE * ROWS #25*25 = 625
 
-pygame.init() #always needed to initialize pygame
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-pygame.display.set_caption("Snake") #title of the window
-clock = pygame.time.Clock() #used for the frame rate
+pygame.init()                                                   #always needed to initialize pygame
+window = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))     #create game window
+pygame.display.set_caption("Snake")                             #title of the window
+clock = pygame.time.Clock()                                     #used for the frame rate
 
 def get_random(limit):
     return random.randint(0, limit-1) * TILE_SIZE
 
+food = pygame.Rect(get_random(COLS), get_random(ROWS), TILE_SIZE, TILE_SIZE)
 snake = []
 snake.append(pygame.Rect(get_random(COLS), get_random(ROWS), TILE_SIZE, TILE_SIZE)) #head
 snake_velocity = (0, 0) #(velocity x, velocity y)
-food = pygame.Rect(get_random(COLS), get_random(ROWS), TILE_SIZE, TILE_SIZE)
 
-while True: #game loop
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: #user clicks the X button in window
-            pygame.quit()
-            exit()
+#game loop
+while True:
+    for event in pygame.event.get():    #listen for all user events (button, key, mouse)
+        if event.type == pygame.QUIT:   #user clicks on x button of window
+            pygame.quit()               #exit pygame
+            exit()                      #terminate the python program completely
+
         if event.type == pygame.KEYDOWN:
         #if event.key == pygame.K_UP or event.key ==  pygame.K_w):
             if event.key in (pygame.K_UP, pygame.K_w):
@@ -52,9 +54,9 @@ while True: #game loop
 
     #draw
     window.fill("black")
+    pygame.draw.rect(window, "yellow", food)
     for snake_part in snake:
         pygame.draw.rect(window, "cyan", snake_part)
-    pygame.draw.rect(window, "yellow", food)
 
-    pygame.display.update()
-    clock.tick(20) #10 frames per second (fps)
+    pygame.display.update()             #refresh game window
+    clock.tick(10)                      #10 frames per second (fps)
